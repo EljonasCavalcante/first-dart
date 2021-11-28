2a6558c266a40f44a415bbe1288aa2a60f57bfbb
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-11-28 18:44:51 -0300

    Feat: creanting condicionais Dart

---
+++ testaCondicionais.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Testando condicionais");
+  int idade = 18;
+  if(idade >= 18) {
+    print("Você é maior de idade, pode entrar!");
+  }
+  else {
+    print('você é menor de idade, não pode entrar!');
+  }
+}
