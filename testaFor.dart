94fc2af4b24c5cb1341cf953197352a9f256edd3
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-11-29 19:38:46 -0300

    Feat: creating FOR dart

---
+++ testaFor.dart
@@ -0,0 +1,8 @@
+void main() {
+  //for(variável; condição; oque fazer com essa variável)
+
+  for (int contador = 0; contador <= 10; contador++) {
+    print("O valor do contador é $contador");
+  }
+  print("Finalizndo o programa");
+}
