6953b2a6aa191c08b92bb9a54b55b60e8d8b14b3
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-11-28 22:19:23 -0300

    feat:creating update

---
+++ exercicioVogaisEConsoantes.dart
@@ -0,0 +1,13 @@
+
+
+
+void main() {
+    String letra = "y";
+
+    //ifs aqui
+    if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+      print("vogal");
+    } else {
+      print("consoante");
+    }
+}
